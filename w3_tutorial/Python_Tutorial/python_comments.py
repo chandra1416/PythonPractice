b7ee3f60d826719f55cdb32da01f190bdc36ea17
chandra1416 <mukanachandra1416@gmail.com>
2022-11-03 11:16:44 +0530

    practice multi line comments

--- w3_tutorial/Python_Tutorial/python_comments.py
+++ w3_tutorial/Python_Tutorial/python_comments.py
@@ -1,3 +1,12 @@
-# single line comments
+# 1: single line comments
 # print("Hello,World")
-print('Hello,Chandra')
+print('single line comment')
+# 2: multi line comments can be applied through add '#' at starting of each line
+# print('chandra')
+# print("sekhar")
+# print('''chandrasekhar''')
+# 3: Python does not really have a syntax for multi line comments.
+# you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+'''This is multiline comment'''
+print('multi line comments')
+
