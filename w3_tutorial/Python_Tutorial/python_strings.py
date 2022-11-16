c349558451e7e394f9e94f34e7cc4f4d6d280d62
chandra1416 <mukanachandra1416@gmail.com>
2022-11-16 23:44:23 +0530

    Practice Python Strings part 6 : Escape Characters

--- w3_tutorial/Python_Tutorial/python_strings.py
+++ w3_tutorial/Python_Tutorial/python_strings.py
@@ -266,3 +266,11 @@ myorder = "I want {} pieces of item {} for {} dollars."
 myorder_1 = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemno, price))
 print(myorder_1.format(quantity, itemno, price))
+print('<----------------------------<<Python - Escape Characters>>--------------------------->')
+# To insert characters that are illegal in a string, use an escape character.
+# An escape character is a backslash \ followed by the character you want to insert.
+# An example of an illegal character is a double quote inside a string that is surrounded by double quotes:
+# txt = "We are the so-called "Vikings" from the north."
+# To fix this problem, use the escape character \":
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
