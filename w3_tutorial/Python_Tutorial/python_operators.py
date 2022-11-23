99f15bcacdfa91500834d7ee5065b0d986495b97
chandra1416 <mukanachandra1416@gmail.com>
2022-11-24 00:24:33 +0530

    Practice Python Operators Part-2

--- w3_tutorial/Python_Tutorial/python_operators.py
+++ w3_tutorial/Python_Tutorial/python_operators.py
@@ -104,13 +104,56 @@ print('not(x > 3 and x < 10) :', not(x > 3 and x < 10))
 print("#######################################")
 print("<--------Identity operators----------->")
 print("#######################################")
+# Identity operators are used to compare the objects, not if they are equal, but if they are actually the same
+# object, with the same memory location:
+print('1. "is ": Returns True if both variables are the same object ')
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print('x : ', x)
+print('y : ', y)
+print('x is z :', x is z)
+# returns True because z is the same object as x
+print('x is y :', x is y)
+# returns False because x is not the same object as y, even if they have the same content
+print('x == y: ', x == y)
+# to demonstrate the difference between "is" and "==": this comparison returns True because x is equal to y
+print('2. "is not": Returns True if both variables are not the same object')
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print('x : ', x)
+print('y : ', y)
+print(x is not z)
+# returns False because z is the same object as x
+print(x is not y)
+# returns True because x is not the same object as y, even if they have the same content
+print(x != y)
+# to demonstrate the difference betweeen "is not" and "!=": this comparison returns False because x is equal to y
 
 print("#######################################")
 print("<--------Membership operators--------->")
 print("#######################################")
+print('1. "in ": Returns True if a sequence with the specified value is present in the object ')
+x = ["apple", "banana"]
+print('x :', x)
+print('Example : "banana" in x :', "banana" in x)
+# returns True because a sequence with the value "banana" is in the list
+print('2. "not in ": Returns True if a sequence with the specified value is present in the object')
+x = ["apple", "banana"]
+print('x :', x)
+print('Example : "pineapple" not in x :', "pineapple" not in x)
+# returns True because a sequence with the value "pineapple" is not in the list
 
 print("#######################################")
 print("<--------Bitwise operators------------>")
 print("#######################################")
-
+print('operator : &, name : AND , Description:Sets each bit to 1 if both bits are 1')
+print('operator : |, name : OR ,Description:Sets each bit to 1 if one of two bits is 1')
+print('operator : ^, name : XOR ,Description:Sets each bit to 1 if only one of two bits is 1')
+print('operator : ~, name : NOT ,Description:Inverts all the bits')
+print('operator : <<, name : Zero fill left shift,Description:Shift left by pushing zeros in from the right and let '
+      'the leftmost bits fall off')
+print('operator : >>, name : Signed right shift , Description:Shift right by pushing copies of the leftmost bit in '
+      'from the left, and let the rightmost bits fall off')
 
