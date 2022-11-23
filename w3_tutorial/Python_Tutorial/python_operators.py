e8ccc6c1bb2e4db3d982130413bcc953cda6f5d3
chandra1416 <mukanachandra1416@gmail.com>
2022-11-23 23:57:29 +0530

    Practice Python Operators Part-1

---
+++ w3_tutorial/Python_Tutorial/python_operators.py
@@ -0,0 +1,116 @@
+print("Operators are used to perform operations on variables and values.")
+# Python divides the operators in the following groups:
+# 1.Arithmetic operators
+# 2.Assignment operators
+# 3.Comparison operators
+# 4.Logical operators
+# 5.Identity operators
+# 6.Membership operators
+# 7.Bitwise operators
+print("#######################################")
+print("<--------Arithmetic operators--------->")
+print("#######################################")
+x = 5
+y = 3
+print("first value is 5 and second value is 3")
+print("1.Addition:", x + y)
+print("2.Subtraction:", x - y)
+print("3.Multiplication:", x * y)
+print("4.Division:", x / y)
+print("5.Modules:", x % y)
+print("6.Exponentiation:", x ** y)
+print("7.Floor division:", x // y)
+
+print("#######################################")
+print("<--------Assignment operators--------->")
+print("#######################################")
+print("a = 5")
+print('operator : =, example a = 5, same as : a = 5')
+a = 5
+print(a)
+#print("a = 5")
+print('operator : +=, example a += 3, same as : a = a + 3')
+a += 3
+print('>> Results : ', a)
+a = 5
+print('operator : -=, example a -= 3, same as : a = a - 3')
+a -= 3
+print('>> Results : ', a)
+a = 5
+print('operator : *=, example a *= 3, same as : a = a * 3')
+a *= 3
+print('>> Results : ', a)
+a = 5
+print('operator : /=, example a /= 3, same as : a = a / 3')
+a /= 3
+print('>> Results : ', a)
+a = 5
+print('operator : %=, example a %= 3, same as : a = a % 3')
+a %= 3
+print('>> Results : ', a)
+a = 5
+print('operator : **=, example a **= 3, same as : a = a ** 3')
+a **= 3
+print('>> Results : ', a)
+a = 5
+print('operator : //=, example a //= 3, same as : a = a // 3')
+a //= 3
+print('>> Results : ', a)
+a = 5
+print('operator : &=, example a &= 3, same as : a = a & 3')
+a &= 3
+print('>> Results : ', a)
+a = 5
+print('operator : |=, example a |= 3, same as : a = a | 3')
+a |= 3
+print('>> Results : ', a)
+a = 5
+print('operator : ^=, example a ^= 3, same as : a = a ^ 3')
+a ^= 3
+print('>> Results : ', a)
+a = 5
+print('operator : <<=, example a <<= 3, same as : a = a << 3')
+a <<= 3
+print('>> Results : ', a)
+a = 5
+print('operator : >>=, example a >> 3, same as : a >> 3')
+a >>= 3
+print('>> Results : ', a)
+
+print("#######################################")
+print("<--------Comparison operators--------->")
+print("#######################################")
+m, n = 5, 3
+print('m = 5 , n = 3')
+print('1. Equal(m == n):', m == n)
+print('2. Not Equal(m != n):', m != n)
+print('3. Greater than(m > n):', m > n)
+print('4. Less then:(m < n)', m < n)
+print('5. Greater than or Equal to(m >= n):', m >= n)
+print('6. Less then or Equal to(m <= n):', m <= n)
+
+print("#######################################")
+print("<---------Logical operators----------->")
+print("#######################################")
+x = 5
+print('x = 5')
+print('1. "and": Returns True if both statements are true')
+print('x > 3 and x < 10 :', x > 3 and x < 10)
+print('2. "or": Returns True if one of the statements is true')
+print('x > 3 or x < 4 :', x > 3 or x < 4)
+print('3. "not": Reverse the result, returns False if the result is true')
+print('not(x > 3 and x < 10) :', not(x > 3 and x < 10))
+
+print("#######################################")
+print("<--------Identity operators----------->")
+print("#######################################")
+
+print("#######################################")
+print("<--------Membership operators--------->")
+print("#######################################")
+
+print("#######################################")
+print("<--------Bitwise operators------------>")
+print("#######################################")
+
+
