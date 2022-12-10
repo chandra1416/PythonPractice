9c4471b8e75a15aef735b5c021c34c0dba678df7
chandra1416 <mukanachandra1416@gmail.com>
2022-12-10 12:25:57 +0530

    Practice Python Lists Part-2 : Access List Items

--- w3_tutorial/Python_Tutorial/python_lists.py
+++ w3_tutorial/Python_Tutorial/python_lists.py
@@ -161,3 +161,37 @@ def myFunc(e):
 cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
 cars.sort(reverse=True, key=myFunc)
 print(cars)
+print('<<<----------------Python - Access List Items---------------------->>>')
+# Access Items : List items are indexed and you can access them by referring to the index number.
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+# Note: The first item has index 0.
+# Negative Indexing :  Negative indexing means start from the end
+# -1 refers to the last item, -2 refers to the second last item etc.
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+# Range of Indexes : You can specify a range of indexes by specifying where to start and where to end the range.
+# You can specify a range of indexes by specifying where to start and where to end the range.
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+# Note: The search will start at index 2 (included) and end at index 5 (not included).
+# Remember that the first item has index 0.
+# By leaving out the start value, the range will start at the first item:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+# By leaving out the end value, the range will go on to the end of the list:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+# Range of Negative Indexes : Specify negative indexes if you want to start the search from the end of the list
+# Specify negative indexes if you want to start the search from the end of the list:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+# Check if Item Exists: To determine if a specified item is present in a list use the in keyword
+# Check if "apple" is present in the list:
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
+
+
+
+
